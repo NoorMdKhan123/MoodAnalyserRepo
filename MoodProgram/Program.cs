6527867f0f3295738b14fc5f16435345febfb84c
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-29 01:15:54 +0530

    Done

---
+++ MoodProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Problem");
+        }
+    }
+}
